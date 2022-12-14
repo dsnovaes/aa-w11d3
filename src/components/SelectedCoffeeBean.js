643a678ba984262a166196c9a7e1dabc139d1fd0
Diego Novaes <dsnovaes1@gmail.com>
2022-12-14 14:46:14 -0800

    phase 2 completed

--- src/components/SelectedCoffeeBean.js
+++ src/components/SelectedCoffeeBean.js
@@ -1,8 +1,10 @@
+import { useEffect } from 'react';
 import {useCoffeeContext} from '../context/CoffeeContext'
 
 const SelectedCoffeeBean = () => {
 
   const { coffeeBean, setCoffeeBeanId } = useCoffeeContext();
+  useEffect(()=>{},[coffeeBean])
 
   return (
     <div className="selected-coffee">
--- src/components/SetCoffeeBean.js
+++ src/components/SetCoffeeBean.js
@@ -1,15 +1,14 @@
 import { useCoffeeContext } from "../context/CoffeeContext";
 
 const SetCoffeeBean = ({ coffeeBeans }) => {
-  const {setCoffeeBeanId} = useCoffeeContext()
+  const {coffeeBean,setCoffeeBeanId} = useCoffeeContext()
   const handleChange = e => {
-    console.log(e.target.value)
-    // setCoffeeBeanId(e.target.value)
+    setCoffeeBeanId(e.target.value)
   }
   return (
     <div className="set-coffee-bean">
       <h2>Select a Coffee Bean</h2>
-      <select name="coffee-bean" onChange={handleChange}>
+      <select name="coffee-bean" onChange={handleChange} value={coffeeBean.id}>
         {coffeeBeans.map(bean => (
           <option
             key={bean.id}
