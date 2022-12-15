a2ded05288e7dc8c4365da5c6ac3334a66f48988
Bikramjit Singh <singhbikramjit222@gmail.com>
2022-12-14 16:06:13 -0800

    Some part of greenhouse

--- greenhouse/src/components/Greenhouse/Greenhouse.js
+++ greenhouse/src/components/Greenhouse/Greenhouse.js
@@ -1,12 +1,15 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
+import { useTheme } from '../../context/ThemeContext';
+
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
 function Greenhouse() {
-
+  const {themeName, setThemeName} = useTheme()
+  console.log(themeName)
   return (
     <section>
       <img  className='greenhouse-img'
--- greenhouse/src/components/Greenhouse/LightSwitch.js
+++ greenhouse/src/components/Greenhouse/LightSwitch.js
@@ -1,8 +1,23 @@
 import './LightSwitch.css';
+import { useTheme } from '../../context/ThemeContext';
+import { useRef } from 'react';
 
 function LightSwitch() {
+  const lightSwitch = useRef()
+  const {themeName, setThemeName} = useTheme()
+  const handleClick = e => {
+    let theme = e.target.innerText.toLowerCase()
+    // console.log(theme)
+    if(theme === 'day') {
+      // lightSwitch.classList.remove("day")
+      setThemeName('day')
+    }else {
+      // lightSwitch.classList.remove("night")b
+      setThemeName('night')
+    }
+  }
   return (
-    <div className="light-switch day">
+    <div className={`light-switch ${themeName}`} onClick={handleClick} ref={lightSwitch}>
       <div className="on">DAY</div>
       <div className="off">NIGHT</div>
     </div>
